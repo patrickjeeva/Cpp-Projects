83fb0ea6967fc046da1bcf2677f5e4bff9995ea2
AbdulMajid1m1 <73154985+AbdulMajid1m1@users.noreply.github.com>
2020-10-20 20:41:29 +0500

    h.cpp

---
+++ print hello.cpp
@@ -0,0 +1,9 @@
+/*use def c++ to run code*/
+#include <iostream> //library for console output
+using namespace std;
+
+main()
+{   //start the program
+    cout << "Hello, World!" << endl; //display name
+    cout<<"hello c++";
+}   //end the program
