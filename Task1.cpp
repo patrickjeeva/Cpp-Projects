0c1d897f61c6c01bf178ad565f20009382215077
Hasnain Ahmed Khan <73159324+HasnainAhmedKhan@users.noreply.github.com>
2020-10-20 12:32:33 +0500

    C++ Output Function

---
+++ Task1.cpp
@@ -0,0 +1,8 @@
+/*Following code is written and compiled in Code::Blocks IDE*/
+#include <iostream> //library for console output
+using namespace std;
+
+main()
+{   //start the program
+    cout << "Syed Mohsin Shah" << endl; //display name
+}   //end the program
